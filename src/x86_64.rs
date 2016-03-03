623260abe6e497b5ecec78c2db9d15d432fcfef0
snf <snf@users.noreply.github.com>
2016-03-03 18:23:27 -0500

    X86_64::disassemble now can return None in case it failed

--- src/x86_64.rs
+++ src/x86_64.rs
@@ -57,21 +57,25 @@ impl Disassemble for X86_64 {
     }
 
     fn disassemble(bytes: &[u8], address: u64) -> Option<Instruction> {
-        let (mnemonic, op_str) =
-            Self::disassemble_arch(Arch::X86_64, bytes, address).unwrap();
+        if let Some(disas) =
+            Self::disassemble_arch(Arch::X86_64, bytes, address)
+        {
+            let (mnemonic, op_str) = disas;
+            let opnds: Vec<(&str, Option<u32>)> =
+                if op_str.trim().is_empty() {
+                    Vec::new()
+                } else {
+                    op_str.split(',')
+                        .map(|s| s.trim())
+                        .map(|s| (s, Self::get_opnd_width(s)))
+                        .collect()
+                };
+            Some(Instruction::new(&mnemonic,
+                                  &opnds))
 
-        let opnds: Vec<(&str, Option<u32>)> =
-            if op_str.trim().is_empty() {
-                Vec::new()
-            } else {
-                op_str.split(',')
-                    .map(|s| s.trim())
-                    .map(|s| (s, Self::get_opnd_width(s)))
-                    .collect()
-            };
-        Some(Instruction::new(&mnemonic,
-                              &opnds))
-
+        } else {
+            None
+        }
     }
 }
 
